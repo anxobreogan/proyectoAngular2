8abbad463428a95e7e7aa718becec2f84d45b649
[Anxo] <[anxobreogan@hotmail.com]>
2019-05-09 10:34:36 +0200

    formulario de alta

--- src/app/components/form-alta/form-alta.component.ts
+++ src/app/components/form-alta/form-alta.component.ts
@@ -11,29 +11,19 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class FormAltaComponent implements OnInit {
 
   producto: Producto = {
-
-
     producto: '',
     precio: '',
     descripcioncorta: '',
     descripcioncompleta: '',
     uuid: localStorage.getItem('uuid'),
     idlocalizacion: ''
-
-
-
   };
 
   edit: boolean = false;
-
   constructor(private productsservice: ProductsService, private router: Router, private acitvatedRoute: ActivatedRoute) { }
-
   ngOnInit() {
-
     const params = this.acitvatedRoute.snapshot.params;
-
     console.log(params);
-
     if (params.idproducto) {
       // console.log(params.idproducto);
       this.productsservice.getProductoId(params.idproducto).subscribe(
@@ -47,20 +37,11 @@ export class FormAltaComponent implements OnInit {
     }
 
   }
-
   anadirProducto() {
-
-
-
     this.productsservice.altaProducto(this.producto).subscribe()
     console.log(this.producto);
-
   };
-
   actualizarProducto() {
     console.log(this.producto);
   }
-
-
-
 }
