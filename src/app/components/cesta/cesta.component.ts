3b62a23746e50b9340334670d5713dfeb6fd3974
[Anxo] <[anxobreogan@hotmail.com]>
2019-05-09 10:27:38 +0200

    codigo cesta

--- src/app/components/cesta/cesta.component.ts
+++ src/app/components/cesta/cesta.component.ts
@@ -11,26 +11,14 @@ import { Router } from '@angular/router';
 })
 export class CestaComponent implements OnInit {
 
-
-
-
   constructor(private productService: ProductsService, private router: Router) { }
-
   public cestas: CestaInterface;
-
   ngOnInit() {
-
     this.verCesta();
-
   }
-
   verCesta() {
     this.productService.getCesta()
       .subscribe((cestas: CestaInterface) => (this.cestas = cestas));
-
     this.router.navigate(['/cesta']);
   }
-
-
-
 }
